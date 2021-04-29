f6f034055b0361409247d8b5cb003feb214878df
Your Name <vyesscenia@mail.fresnostate.edu>
2021-04-28 23:33:06 -0700

    finished Structured English

---
+++ group_project.py
@@ -0,0 +1,32 @@
+"""
+
+This program will allow the user to make transactions to a savings account.
+
+The account initially has a balance of $2,000. The first option is to make the depostit
+in the savings account of any amount.The second option allows the userto make a withdrawal
+that is less or equal to their current balance. The third option is to obtain the balnce.
+The fourht option is to quit. There will four functions that will allow the user to make 
+transactions to their savings account. Function one will calculate the amount for the first option,
+functions two will calculate the amount for option two,functiona three will print the amount
+for options three, and function four will end the user's transactions. 
+
+
+function1 will add deposit to current balance: $2,000
+
+function2 will withdraw money from the savings account, will output "denied" if maximum withdrawal
+limit is reached
+
+function3 will print the current balance
+
+function4 will allow the user to quit
+
+if the user makes a deposit, then it will output to the user "Deposit processed"
+
+if the user attempts to make a withdrawal larger than the current balance, it will output "Denied.
+Maximum withdrawal is ___"
+
+if the user request balance of their savings account, it will output to the user 
+"Balance ___", or else if user did not input the proper number, it will output
+"Error. You did not enter proper number."
+
+"""
